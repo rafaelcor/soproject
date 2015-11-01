c7882656129386aae852d62883a05eb34d51f245
Rafael Cordano <rafael.cordano@gmail.com>
2015-11-01 15:30:30 -0300

    You cannot save on the past now

--- app/form/reservar_cb.sh
+++ app/form/reservar_cb.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 function procesar_cb {
-	#echo Procesando
-	local fechaReserva=${txtValue[$(expr $txtI - 2)]}
-	local horaInicio=${txtValue[$(expr $txtI - 1)]}
-	local horaFin=${txtValue[$(expr $txtI)]}
+	echo Procesando
+	fechaReserva=${txtValue[$(expr $txtI - 2)]}
+	horaInicio=${txtValue[$(expr $txtI - 1)]}
+	horaFin=${txtValue[$(expr $txtI)]}
 	
 	if [[ "$fechaReserva" =~ [0-9][0-9]|[0-9]-[0-9][0-9]|[0-9]-[0-9][0-9][0-9][0-9] ]] && [[ "$horaInicio" =~ [0-9][0-9]|[0-9]:[0-9][0-9] ]] && [[ "$horaFin" =~ [0-9][0-9]|[0-9]:[0-9][0-9] ]] && test $(abs $(expr $(date -ud "$horaInicio" +"%s") - $(date -ud "$horaFin" +"%s"))) -le 21600 && test $(date -ud "$horaFin" +"%H") -ne 2 && test $(date -ud "$horaInicio" +"%H") -ne 2 ;then
 		#&& test $(echo "$fechaReserva" | tr '-' "\n") -ge $(expr $(date +%Y))
 		echo "Horas válidas"
-		local fechaYTemp=($(echo "$fechaReserva" | tr '-' "\n"))
-		if test "${fechaYTemp[2]}" -ge $(date +%Y);then
+		fechaYTemp=($(echo "$fechaReserva" | tr '-' "\n"))
+		fechaTemp=$(date -d ${fechaYTemp[2]}-${fechaYTemp[1]}-${fechaYTemp[0]} +"%Y%m%d")
+		if [ "$fechaTemp" -ge $(date +"%Y%m%d") ];then
 			echo "if defin"
 		else
 			echo "dentroifelse"
 		fi
 		
 	else
-		firstText
+		echo "No"
 	fi
 	
 }
@@ -26,6 +27,8 @@ function cancelar_cb {
 	echo Cancelado
 }
 
-function reingresarFecha_cb {
+function volver_cb {
+	txtI=0 # Fix 0
 	firstText
+	firstButton
 }
