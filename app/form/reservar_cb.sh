bff0742cb0f10589f7e5a8e1e4cb1648c4bc795e
francoyo1998 <fprofeti98@gmail.com>
2015-11-01 14:59:16 -0300

    alcance de variables corregido

--- app/form/reservar_cb.sh
+++ app/form/reservar_cb.sh
@@ -2,14 +2,14 @@
 
 function procesar_cb {
 	#echo Procesando
-	fechaReserva=${txtValue[$(expr $txtI - 2)]}
-	horaInicio=${txtValue[$(expr $txtI - 1)]}
-	horaFin=${txtValue[$(expr $txtI)]}
+	local fechaReserva=${txtValue[$(expr $txtI - 2)]}
+	local horaInicio=${txtValue[$(expr $txtI - 1)]}
+	local horaFin=${txtValue[$(expr $txtI)]}
 	
 	if [[ "$fechaReserva" =~ [0-9][0-9]|[0-9]-[0-9][0-9]|[0-9]-[0-9][0-9][0-9][0-9] ]] && [[ "$horaInicio" =~ [0-9][0-9]|[0-9]:[0-9][0-9] ]] && [[ "$horaFin" =~ [0-9][0-9]|[0-9]:[0-9][0-9] ]] && test $(abs $(expr $(date -ud "$horaInicio" +"%s") - $(date -ud "$horaFin" +"%s"))) -le 21600 && test $(date -ud "$horaFin" +"%H") -ne 2 && test $(date -ud "$horaInicio" +"%H") -ne 2 ;then
 		#&& test $(echo "$fechaReserva" | tr '-' "\n") -ge $(expr $(date +%Y))
 		echo "Horas válidas"
-		fechaYTemp=($(echo "$fechaReserva" | tr '-' "\n"))
+		local fechaYTemp=($(echo "$fechaReserva" | tr '-' "\n"))
 		if test "${fechaYTemp[2]}" -ge $(date +%Y);then
 			echo "if defin"
 		else
