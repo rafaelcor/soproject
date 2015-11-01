1528fd4c2c15981a5542128cdf4d411564427f80
Rafael Cordano <rafael.cordano@gmail.com>
2015-11-01 13:03:14 -0300

    fixed year

--- app/form/reservar_cb.sh
+++ app/form/reservar_cb.sh
@@ -6,8 +6,16 @@ function procesar_cb {
 	horaInicio=${txtValue[$(expr $txtI - 1)]}
 	horaFin=${txtValue[$(expr $txtI)]}
 	
-	if test $(abs $(expr $(date -ud "$horaInicio" +"%s") - $(date -ud "$horaFin" +"%s"))) -le 21600 && test $(date -ud "$horaFin" +"%H") -ne 2 && test $(date -ud "$horaInicio" +"%H") -ne 2;then
+	if [[ "$fechaReserva" =~ [0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9] ]] && [[ "$horaInicio" =~ [0-9][0-9]:[0-9][0-9] ]] && [[ "$horaFin" =~ [0-9][0-9]:[0-9][0-9] ]] && test $(abs $(expr $(date -ud "$horaInicio" +"%s") - $(date -ud "$horaFin" +"%s"))) -le 21600 && test $(date -ud "$horaFin" +"%H") -ne 2 && test $(date -ud "$horaInicio" +"%H") -ne 2 ;then
+		#&& test $(echo "$fechaReserva" | tr '-' "\n") -ge $(expr $(date +%Y))
 		echo "Horas válidas"
+		fechaYTemp=($(echo "$fechaReserva" | tr '-' "\n"))
+		if test "${fechaYTemp[2]}" -ge $(date +%Y);then
+			echo "if defin"
+		else
+			echo "dentroifelse"
+		fi
+		
 	else
 		echo "No"
 	fi
