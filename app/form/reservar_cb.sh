3adeffc1ce39f5125d5c9398a9d27b31e740db58
Rafael Cordano <rafael.cordano@gmail.com>
2015-11-01 13:17:20 -0300

    Fixed regex fecha

--- app/form/reservar_cb.sh
+++ app/form/reservar_cb.sh
@@ -6,7 +6,7 @@ function procesar_cb {
 	horaInicio=${txtValue[$(expr $txtI - 1)]}
 	horaFin=${txtValue[$(expr $txtI)]}
 	
-	if [[ "$fechaReserva" =~ [0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9] ]] && [[ "$horaInicio" =~ [0-9][0-9]:[0-9][0-9] ]] && [[ "$horaFin" =~ [0-9][0-9]:[0-9][0-9] ]] && test $(abs $(expr $(date -ud "$horaInicio" +"%s") - $(date -ud "$horaFin" +"%s"))) -le 21600 && test $(date -ud "$horaFin" +"%H") -ne 2 && test $(date -ud "$horaInicio" +"%H") -ne 2 ;then
+	if [[ "$fechaReserva" =~ [0-9][0-9]|[0-9]-[0-9][0-9]|[0-9]-[0-9][0-9][0-9][0-9] ]] && [[ "$horaInicio" =~ [0-9][0-9]:[0-9][0-9] ]] && [[ "$horaFin" =~ [0-9][0-9]:[0-9][0-9] ]] && test $(abs $(expr $(date -ud "$horaInicio" +"%s") - $(date -ud "$horaFin" +"%s"))) -le 21600 && test $(date -ud "$horaFin" +"%H") -ne 2 && test $(date -ud "$horaInicio" +"%H") -ne 2 ;then
 		#&& test $(echo "$fechaReserva" | tr '-' "\n") -ge $(expr $(date +%Y))
 		echo "Horas válidas"
 		fechaYTemp=($(echo "$fechaReserva" | tr '-' "\n"))
